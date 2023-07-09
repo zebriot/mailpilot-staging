481557322657cf9f1fa70740f22121abe09b544f
Siddharth Sharma <zebriot@gmail.com>
2023-07-10 01:58:23 +0530

    API KEYS Fix

--- next.config.js
+++ next.config.js
@@ -1,4 +1,6 @@
 module.exports = {
-
-  };
-  
+  env: {
+    OPENAI_API_KEY: "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",
+    SCRAPPER_API_KEY: "b5f8b28716404c8b82e291b33a039ab7",
+  },
+};
