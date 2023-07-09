6bdbdd18e540036cd0240d99cfb8c8fcfe5b8cdd
Siddharth Sharma <zebriot@gmail.com>
2023-07-10 02:04:49 +0530

    API KEY FIX

--- next.config.js
+++ next.config.js
@@ -1,6 +1,8 @@
+require("dotenv").config();
+
 module.exports = {
   env: {
-    OPENAI_API_KEY: "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",
-    SCRAPPER_API_KEY: "b5f8b28716404c8b82e291b33a039ab7",
+    OPENAI_API_KEY: process.env.OPENAI_API_KEY,
+    SCRAPPER_API_KEY: process.env.SCRAPPER_API_KEY,
   },
 };
